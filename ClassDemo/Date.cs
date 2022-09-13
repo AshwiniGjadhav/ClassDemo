5d68ddbd215d8c9f8e002c3593655458707a9fd8
Ashwini <Ashwini@LAPTOP-FQL7UM83>
2022-09-13 17:32:29 +0530

    Add project files.

---
+++ ClassDemo/Date.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    public class Date
+    {
+        int day, year;
+        string month;
+
+        public void AssignValue()
+        {
+            day=13;
+            year=2022;
+            month = "sept";
+        }
+
+        public string display()
+        {
+            return $" Date is :{day}/{month}/{year}";
+        }
+    }
+}
---
+++ ClassDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Date date = new Date();
+
+            date.AssignValue();
+
+            Console.WriteLine(date.display());
+        }
+    }
+}
